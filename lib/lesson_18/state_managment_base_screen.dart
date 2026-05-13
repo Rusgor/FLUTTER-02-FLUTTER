5278dbaa44a1a02f1867eadc1050edfc071c2aec
Rusgor <it.rusgor@gmail.com>
2026-05-13 18:03:21 +0300

    feat: setup lesson 18 state management structure

---
+++ lib/lesson_18/state_managment_base_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StateManagmentBaseScreen extends StatelessWidget {
+  const StateManagmentBaseScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('State Management')),
+      body: const Center(child: Text('HW-18')),
+    );
+  }
+}
